7c3972ec7a23bc958ba49d004fa6567f7cdba7fc
Fatih <fatihmertdogancan@hotmail.com>
2020-12-16 21:10:49 +0300

    add rotuer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import routes from './routes'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  routes,
   render: h => h(App),
 }).$mount('#app')
