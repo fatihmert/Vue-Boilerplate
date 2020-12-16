1f13e6787bd9fe2fe0f068e066f05a5c44f2c5d0
Fatih <fatihmertdogancan@hotmail.com>
2020-12-16 21:14:15 +0300

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import routes from './routes'
+import store from './store'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   routes,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+  data(){
+    return {
+      loading: true,
+    };
+  },
+  methods: {
+
+  },
+  mounted () {
+
+  },
+
+}
+</script>
